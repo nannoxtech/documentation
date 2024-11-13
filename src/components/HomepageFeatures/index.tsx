f33c51610772ef79b6e5bafc6df325f516f7a9a7
Yubisel Vega <yubiselv@gmail.com>
2024-11-13 13:28:05 -0300

    feat: update homepage feature descriptions and comment out unused elements in the header

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,8 +14,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui veniam saepe cum vitae necessitatibus omnis sint aut! Voluptate accusantium voluptatum provident expedita delectus magni officia officiis libero dignissimos deserunt!
       </>
     ),
   },
@@ -24,8 +23,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui veniam saepe cum vitae necessitatibus omnis sint aut! Voluptate accusantium voluptatum provident expedita delectus magni officia officiis libero dignissimos deserunt!
       </>
     ),
   },
@@ -34,8 +32,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui veniam saepe cum vitae necessitatibus omnis sint aut! Voluptate accusantium voluptatum provident expedita delectus magni officia officiis libero dignissimos deserunt!
       </>
     ),
   },
